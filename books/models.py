e081eb4003bec82895d9689ef3c35a13704ab8ea
siamak kamanabroo <siamak.kamanabroo@gmail.com>
2024-09-21 18:59:52 +0330

    Update models

--- books/models.py
+++ books/models.py
@@ -1,26 +1,68 @@
 from django.db import models
+from datetime import date
 
 
 class Author(models.Model):
-    name = models.CharField(max_length=100)
 
-    def __str__(self):
+    name = models.CharField(
+        max_length=250,
+        db_index=True
+        )
+    email = models.EmailField(
+        'Email',
+        unique=True
+        )
+    created_at = models.DateTimeField(
+        'Created time',
+        auto_now_add=True
+        )
+
+    def __str__(self) -> str:
         return self.name
 
+
+class Book(models.Model):
+
+    title = models.CharField(
+        'Book Title',
+        max_length=250,
+        db_index=True
+        )
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.CASCADE,
+        verbose_name='Author',
+        )
+    value = models.BigIntegerField(
+        'Value',
+    )
+    quantity = models.IntegerField(
+        'Numbers of books'
+    )
+    date_published = models.DateField(
+        'Publish Date',
+        default=date.today
+    )
+    genre = models.CharField(
+        'Genre',
+        )
+    updated_at = models.DateField(
+        'Last Update Date',
+        auto_now=True,
+    )
+    created_at = models.DateField(
+        'Created Time',
+        auto_now_add=True
+    )
+
+    def __str__(self) -> str:
+        return f"{self.title} - {self.author}"
+    
+    def details(self) -> str:
+        return f"Price: {self.value}, Published at: {self.date_published}, Quantity: {self.quantity}"
 
 class Genre(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
-
-
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.ManyToManyField(Author)
-    date_published = models.DateField()
-    genre = models.ManyToManyField(Genre)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-
-    def __str__(self):
-        return self.title
