8f1a862a5dcebe58f37bd9725da3b6ac7e5a6520
siamak kamanabroo <siamak.kamanabroo@gmail.com>
2024-09-26 13:10:25 +0330

    change admin codes

--- books/admin.py
+++ books/admin.py
@@ -1,7 +1,36 @@
 from django.contrib import admin
 from .models import Book
-# from .models import Author
+
 
 # Register your models here.
-# admin.site.register(Author)
-admin.site.register(Book)
+@admin.register(Book)
+class bookAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'title',
+        'author',
+        'genre',
+        'value',
+    ]
+
+    sortable_by = [
+        'id',
+    ]
+
+    list_filter = [
+        'author_country',
+    ]
+
+    list_editable = [
+        'title',
+        'author',
+        'value',
+        'genre',
+    ]
+
+    search_fields = [
+        'title',
+        'author',
+        'value',
+        'author_country',
+    ]
